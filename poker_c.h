7f66434079a4d7f07861ddb1405b4b3ea91a3e9c
Yura Iontsev <yiontsev@gmail.com>
2017-11-23 17:08:15 +0300

    Update poker_c.h

--- poker_c.h
+++ poker_c.h
@@ -1,19 +1,19 @@
 // to-do
-// добавить проверку на последовательность от 10 до короля!
+
 void print_header();
-// Перетасовка
+
 void shuffle(int deck[][13]);
-// Сдача игрокам
+
 void deal(int deck[][13], int p1[][13], int p2[][13]);
 void deal_after_cards_change(int deck[][13], int p2[][13]);
-// Печать сдачи
+
 void print_deal(const char *face[], const char *suit[], int p[][13]);
 void print_deal_after_resubmission(int p[][13], const char *face[], const char *suit[], int assess);
-// Количество повтряющихся карт у игроа
+
 void repeat_card(int p[][13], int repeat_face[], int repeat_suit[]);
 void repeat_card_after_resubmission(int p[][13], int repeat_face[], int repeat_suit[], int assess);
 
-// Комбинации на номиналах карты
+
 int combination_on_face(int repeat_face[], const char *face[]);
 int combination_on_suit(int repeat_suit[]);
 int combination_on_sequence(int repeat_face[]);
